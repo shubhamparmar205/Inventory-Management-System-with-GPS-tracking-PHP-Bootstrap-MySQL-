1ea6cc0ad92ad9c1de7344be45c15433d9649eee
Shubham Parmar <85212709+shubhamparmar205@users.noreply.github.com>
2024-04-30 12:14:55 +0530

    Add files via upload

---
+++ layouts/admin_menu.php
@@ -0,0 +1,50 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>User Management</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Manage Groups</a> </li>
+      <li><a href="users.php">Manage Users</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categories</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Products</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Manage Products</a> </li>
+       <li><a href="add_product.php">Add Products</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-credit-card"></i>
+       <span>Sales</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Manage Sales</a> </li>
+         <li><a href="add_sale.php">Add Sale</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="truck_driver.php" >
+      <i class="glyphicon glyphicon-map-marker"></i>
+      <span>Track Driver</span>
+    </a>
+  </li>
+</ul>
---
+++ layouts/driver.php
@@ -0,0 +1,8 @@
+<ul>
+  <li>
+    <a href="loc.php">
+      <i class="glyphicon glyphicon-map-marker"></i>
+      <span>Show Location</span>
+    </a>
+  </li>
+</ul>
